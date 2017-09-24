d710fb937038e6ed6567e17aa6773ea4f4ceac49
noyahking <32081616+noyahking@users.noreply.github.com>
2017-09-24 11:37:07 +0800

    Initial commit

---
+++ _site/javascript/main.js
@@ -0,0 +1,19 @@
+// Load the DOM
+$(document).ready(function(){
+
+    //Log
+    console.log('Ready!');
+
+    // Click the hamburger icon
+    $('.toggle-mobile-menu').click(function(){
+
+      // Show mobile menu
+      $('#mobile-menu').toggleClass('mobile-menu-visible');
+
+    });
+    // Show the mobile menu
+
+    // Clicking the cross icon
+
+    // Close the mobile menu
+  })
